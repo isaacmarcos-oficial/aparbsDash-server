b15e23a2795acc394c984829bb4f48e18a58ec8d
Isaac Marcos <isaacmarcos@scdesign.com.br>
2024-01-22 17:03:42 -0300

    conection to vercel

---
+++ dist/Inputs/ClientInput.js
@@ -0,0 +1,117 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EditClientInput = exports.CreateClientInput = void 0;
+const type_graphql_1 = require("type-graphql");
+let CreateClientInput = class CreateClientInput {
+};
+exports.CreateClientInput = CreateClientInput;
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], CreateClientInput.prototype, "name", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], CreateClientInput.prototype, "phone", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], CreateClientInput.prototype, "serviceOrder", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], CreateClientInput.prototype, "vehicle", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Date)
+], CreateClientInput.prototype, "dischargeDate", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentToday", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentThreeDays", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentSevenDays", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentOneMonth", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentThreeMonths", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], CreateClientInput.prototype, "sentSixMonths", void 0);
+exports.CreateClientInput = CreateClientInput = __decorate([
+    (0, type_graphql_1.InputType)()
+], CreateClientInput);
+let EditClientInput = class EditClientInput {
+};
+exports.EditClientInput = EditClientInput;
+__decorate([
+    (0, type_graphql_1.Field)((type) => type_graphql_1.ID),
+    __metadata("design:type", String)
+], EditClientInput.prototype, "id", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], EditClientInput.prototype, "name", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], EditClientInput.prototype, "phone", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], EditClientInput.prototype, "serviceOrder", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], EditClientInput.prototype, "vehicle", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Date)
+], EditClientInput.prototype, "dischargeDate", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentToday", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentThreeDays", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentSevenDays", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentOneMonth", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentThreeMonths", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Boolean)
+], EditClientInput.prototype, "sentSixMonths", void 0);
+exports.EditClientInput = EditClientInput = __decorate([
+    (0, type_graphql_1.InputType)()
+], EditClientInput);
