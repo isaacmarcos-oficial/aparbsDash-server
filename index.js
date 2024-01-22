deab2116f26d65439748731dd38d285f8bf32f71
Isaac Marcos <isaacmarcos@scdesign.com.br>
2024-01-22 15:54:21 -0300

    :fix: create build to deploy

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const path_1 = __importDefault(require("path"));
+require("dotenv").config({ path: ".env" });
+require("./mongodb/connect");
+const type_graphql_1 = require("type-graphql");
+const apollo_server_1 = require("apollo-server");
+const ClientResolver_1 = require("./Resolvers/ClientResolver");
+async function main() {
+    const schema = await (0, type_graphql_1.buildSchema)({
+        resolvers: [ClientResolver_1.ClientResolver],
+        emitSchemaFile: path_1.default.resolve(__dirname, "scheme.ggl"),
+    });
+    console.log();
+    const server = new apollo_server_1.ApolloServer({
+        schema,
+        cors: {
+            origin: process.env.FRONTEND_ENDPOINT,
+            credentials: true
+        }
+    });
+    const { url } = await server.listen();
+    console.log("Server running on " + url);
+}
+main();
---
+++ mongodb/Models/Client.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ClientMongo = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
+const schema = new mongoose_1.default.Schema({
+    name: String,
+    phone: String,
+    serviceOrder: Number,
+    vehicle: String,
+    dischargeDate: Date,
+    sentToday: Boolean,
+    sentThreeDays: Boolean,
+    sentSevenDays: Boolean,
+    sentOneMonth: Boolean,
+    sentThreeMonths: Boolean,
+    sentSixMonths: Boolean,
+});
+exports.ClientMongo = mongoose_1.default.model("Client", schema);
